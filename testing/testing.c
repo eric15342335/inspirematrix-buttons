bfefd49bebeece02280738582f9c8f69127f06c2
Cheng Ho Ming, Eric <eric310@connect.hku.hk>
2024-07-23 19:02:50 +0800

    testing buzzer musics!

--- testing/testing.c
+++ testing/testing.c
@@ -4,10 +4,11 @@
 #include "colors.h"
 #include "ws2812b_simple.h"
 #include "ch32v003_GPIO_branchless.h"
+#include "music.h"
 
 int main(void) {
     SystemInit();
-    ADC_init();
+    GPIO_ADCinit();
     while(1) {
         for (int i = 0; i < NUM_LEDS; i++) {
             clear();
@@ -20,6 +21,7 @@ int main(void) {
             printf("PD6: %d\n", GPIO_analogRead(GPIO_Ain6_D6));
             printf("PA1: %d\n", GPIO_analogRead(GPIO_Ain1_A1));
             printf("PA2: %d\n", GPIO_analogRead(GPIO_Ain0_A2));
+            playMusic((noterange_t){i % 13, i % 13 + 1});
             /*
             act: pa2, gpio possible
             */

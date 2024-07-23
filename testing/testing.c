16662d1858efa868d9eb1a09672b57f28fd520e4
Cheng Ho Ming, Eric <eric310@connect.hku.hk>
2024-07-23 18:23:31 +0800

    testing stuffs

---
+++ testing/testing.c
@@ -0,0 +1,31 @@
+#define WS2812BSIMPLE_IMPLEMENTATION
+#include <stdio.h>
+#include "driver.h"
+#include "colors.h"
+#include "ws2812b_simple.h"
+#include "ch32v003_GPIO_branchless.h"
+
+int main(void) {
+    SystemInit();
+    ADC_init();
+    while(1) {
+        for (int i = 0; i < NUM_LEDS; i++) {
+            clear();
+            WS2812BSimpleSend(GPIOC, 1, (uint8_t *)led_array, NUM_LEDS * 3);
+            Delay_Ms(300);
+            printf("Hello, World! %d\n", i);
+            printf("PC4: %d\n", GPIO_analogRead(GPIO_Ain2_C4));
+            printf("PD4: %d\n", GPIO_analogRead(GPIO_Ain7_D4));
+            printf("PD5: %d\n", GPIO_analogRead(GPIO_Ain5_D5));
+            printf("PD6: %d\n", GPIO_analogRead(GPIO_Ain6_D6));
+            printf("PA1: %d\n", GPIO_analogRead(GPIO_Ain1_A1));
+            printf("PA2: %d\n", GPIO_analogRead(GPIO_Ain0_A2));
+            /*
+            act: pa2, gpio possible
+            */
+            fill_color((color_t){30, 0, 0});
+            WS2812BSimpleSend(GPIOC, 1, (uint8_t *)led_array, NUM_LEDS * 3);
+            Delay_Ms(300);
+        }
+    }
+}
